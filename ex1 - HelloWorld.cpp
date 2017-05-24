3ad47c3a21417636cc6c8db69e97a78c808fa6b0
kabbesj <kabbesj@gmail.com>
2017-05-23 22:19:24 -0500

    Add files via upload

---
+++ ex1 - HelloWorld.cpp
@@ -0,0 +1,11 @@
+//HackerRank
+//https://www.hackerrank.com/challenges/cpp-hello-world
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
---
+++ ex6 - function.cpp
@@ -0,0 +1,42 @@
+//HackerRank
+//https://www.hackerrank.com/domains/cpp/cpp-introduction
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int a, int b, int c, int d){
+    
+  int array[] = {a,b,c,d};
+  int largest;
+  int swap;
+    
+//Find largest number by using bubble sort 
+ for(int i=0;i<4;i++){
+      
+      for(int x=0;x<3;x++){
+          if(array[x] > array[x+1]){
+              swap = array[x];
+              array[x] = array[x+1];
+              array[x+1] = swap;
+          }
+      }
+  }
+    
+    largest = array[3];
+    return largest;
+    
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
